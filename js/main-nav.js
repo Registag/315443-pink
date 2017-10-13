60a947c6b9bb45aa141b0198f400a30ac18f35f0
nastya08 <nastyaivanova27@gmail.com>
2017-10-13 13:11:24 +0300

    Исправление замечаний

--- js/main-nav.js
+++ js/main-nav.js
@@ -1,18 +1,22 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var header = document.querySelector(".page-header");
+var headerWrap = document.querySelector(".page-header__wrap");
 
 navMain.classList.remove('main-nav--nojs');
 header.classList.remove('page-header--nojs');
+headerWrap.classList.remove('page-header__wrap--nojs');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
     header.classList.add('page-header--opened');
+    headerWrap.classList.add('page-header__wrap--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
     header.classList.remove('page-header--opened');
+    headerWrap.classList.remove('page-header__wrap--opened');
   }
 });
